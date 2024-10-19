699ebaed6029945ee79b23cd011ab38a2ae63920
Simon Dai <simon6752@gmail.com>
2024-10-18 20:20:31 -0700

    add macos for flutter_gemma

---
+++  macos/Classes/FlutterGemmaPlugin.swift
@@ -0,0 +1,82 @@
+import Flutter
+import UIKit
+
+public class FlutterGemmaPlugin: NSObject, FlutterPlugin {
+   
+    private var inferenceModel: InferenceModel?
+    private var eventSink: FlutterEventSink?
+
+    // This static function correctly initializes the plugin with the Flutter engine
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_gemma", binaryMessenger: registrar.messenger())
+        let instance = FlutterGemmaPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        
+        let eventChannel = FlutterEventChannel(name: "flutter_gemma_stream", binaryMessenger: registrar.messenger())
+        eventChannel.setStreamHandler(instance)
+    }
+
+    // This method correctly handles method calls from Flutter
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "init":
+            if let arguments = call.arguments as? [String: Any], let maxTokens = arguments["maxTokens"] as? Int, let temperature = arguments["temperature"] as? Float, let randomSeed = arguments["randomSeed"] as? Int, let topK = arguments["topK"] as? Int {
+                inferenceModel = InferenceModel(maxTokens: maxTokens, temperature: temperature, randomSeed: randomSeed, topK: topK)
+                result(true)
+            } else {
+                result(FlutterError(code: "ERROR", message: "Failed to initialize gemma", details: nil))
+            }
+        case "getGemmaResponse":
+            if let arguments = call.arguments as? [String: Any], let prompt = arguments["prompt"] as? String {
+                do {
+                    if let response = try inferenceModel?.generateResponse(prompt: prompt) {
+                        result(response)
+                    } else {
+                        result(FlutterError(code: "UNAVAILABLE", message: "Inference model could not generate a response", details: nil))
+                    }
+                } catch {
+                    result(FlutterError(code: "ERROR", message: "Failed to generate response: \(error.localizedDescription)", details: nil))
+                }
+            } else {
+                result(FlutterError(code: "BAD_ARGS", message: "Bad arguments for 'getGemmaResponse' method", details: nil))
+            }
+        case "getGemmaResponseAsync":
+            if let arguments = call.arguments as? [String: Any], let prompt = arguments["prompt"] as? String {
+                do {
+                    try inferenceModel?.generateResponseAsync(prompt: prompt, progress: { partialResponse, error in
+                        DispatchQueue.main.async {
+                            if let error = error {
+                                self.eventSink?(FlutterError(code: "ERROR", message: "Error during getting Gemma response", details: error.localizedDescription))
+                            } else if let partialResponse = partialResponse {
+                                self.eventSink?(partialResponse)
+                            }
+                        }
+                    }, completion: {
+                        DispatchQueue.main.async {
+                            self.eventSink?(nil)
+                            result(nil)
+                        }
+                    })
+                } catch {
+                    result(FlutterError(code: "ERROR", message: "Failed to get async gemma response", details: error.localizedDescription))
+                }
+            } else {
+                result(FlutterError(code: "BAD_ARGS", message: "Bad arguments for 'getGemmaResponseAsync' method", details: nil))
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+}
+
+extension FlutterGemmaPlugin: FlutterStreamHandler {
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil
+    }
+}
---
+++  macos/Classes/InferenceModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+import MediaPipeTasksGenAI
+
+final class InferenceModel {
+    private var inference: LlmInference!
+
+    init(maxTokens: Int, temperature: Float, randomSeed: Int, topK: Int) {
+        let fileManager = FileManager.default
+        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let filePath = documentDirectory.appendingPathComponent("model.bin").path
+
+        let llmOptions = LlmInference.Options(modelPath: filePath)
+        llmOptions.maxTokens = maxTokens
+        llmOptions.temperature = temperature
+        llmOptions.randomSeed = randomSeed
+        llmOptions.topk = topK
+        self.inference = try! LlmInference(options: llmOptions)
+    }
+
+    func generateResponse(prompt: String) throws -> String {
+        return try inference.generateResponse(inputText: prompt)
+    }
+    
+    func generateResponseAsync(prompt: String, progress: @escaping (_ partialResponse: String?, _ error: Error?) -> Void, completion: @escaping (() -> Void)) throws {
+        do {
+            try inference.generateResponseAsync(inputText: prompt, progress: progress, completion: completion)
+        } catch {
+            throw error
+        }
+    }
+}

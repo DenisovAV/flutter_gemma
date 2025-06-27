4c57ada91a39ca66cfe93dc0003a4cf39da6dbe1
Csongor Vogel <15221068+gerfalcon@users.noreply.github.com>
2025-06-27 21:12:32 +0400

    Removes unused imports

--- lib/mobile/flutter_gemma_mobile.dart
+++ lib/mobile/flutter_gemma_mobile.dart
@@ -4,14 +4,11 @@ import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_gemma/core/extensions.dart';
 import 'package:flutter_gemma/core/model.dart';
-import 'package:flutter_gemma/core/chat.dart'; // Добавляем import для InferenceChat
 import 'package:flutter_gemma/pigeon.g.dart';
 import 'package:large_file_handler/large_file_handler.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import '../flutter_gemma.dart';
 
@@ -52,21 +49,18 @@ class FlutterGemma extends FlutterGemmaPlugin {
 
     final completer = _initCompleter = Completer<InferenceModel>();
 
-    final (
-    isModelInstalled,
-    isLoraInstalled,
-    File? modelFile,
-    File? loraFile
-    ) = await (
-    modelManager.isModelInstalled,
-    modelManager.isLoraInstalled,
-    modelManager._modelFile,
-    modelManager._loraFile,
+    final (isModelInstalled, isLoraInstalled, File? modelFile, File? loraFile) =
+        await (
+      modelManager.isModelInstalled,
+      modelManager.isLoraInstalled,
+      modelManager._modelFile,
+      modelManager._loraFile,
     ).wait;
 
     if (!isModelInstalled || modelFile == null) {
       completer.completeError(
-        Exception('Gemma Model is not installed yet. Use the `modelManager` to load the model first'),
+        Exception(
+            'Gemma Model is not installed yet. Use the `modelManager` to load the model first'),
       );
       return completer.future;
     }

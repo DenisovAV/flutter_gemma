e4698b5a55d40412f84017052abec370eebe7cef
Sasha Denisov <denisov.shureg@gmail.com>
2026-01-02 17:01:25 +0100

    Add missing flutter_gemma_plugin.h header for Windows build
    - Create flutter_gemma_plugin.h with class declaration
    - Update flutter_gemma_plugin.cpp to include the header
    - Fixes C1083 error: Cannot open include file

--- windows/flutter_gemma_plugin.cpp
+++ windows/flutter_gemma_plugin.cpp
@@ -4,22 +4,9 @@
 // The actual implementation is in Dart (FlutterGemmaDesktop) using gRPC
 // to communicate with a Kotlin/JVM server process.
 
-#include <flutter/plugin_registrar_windows.h>
+#include "flutter_gemma/flutter_gemma_plugin.h"
 
 namespace flutter_gemma {
-
-// Placeholder plugin class - actual implementation is in Dart
-class FlutterGemmaPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
-
-  FlutterGemmaPlugin();
-  virtual ~FlutterGemmaPlugin();
-
-  // Disallow copy and assign.
-  FlutterGemmaPlugin(const FlutterGemmaPlugin&) = delete;
-  FlutterGemmaPlugin& operator=(const FlutterGemmaPlugin&) = delete;
-};
 
 // static
 void FlutterGemmaPlugin::RegisterWithRegistrar(

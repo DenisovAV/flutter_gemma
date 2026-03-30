61beb3234ff1849a349e43ccbf19f71c3da3c1a8
M.B. <BlueRoyal@users.noreply.github.com>
2026-03-30 22:13:50 +0200

    Update dart_vector_store_repository_stub.dart

--- lib/core/infrastructure/dart_vector_store_repository_stub.dart
+++ lib/core/infrastructure/dart_vector_store_repository_stub.dart
@@ -1,8 +1,11 @@
 import '../services/vector_store_repository.dart';
 
-VectorStoreRepository createDartVectorStoreRepository() {
-  throw UnsupportedError(
-    'DartVectorStoreRepository is not supported on web. '
-    'Use WebVectorStoreRepository instead.',
-  );
+class DartVectorStoreRepository implements VectorStoreRepository {
+  DartVectorStoreRepository([dynamic db]) {
+    throw UnsupportedError('DartVectorStoreRepository is not available on web.');
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) =>
+      throw UnsupportedError('DartVectorStoreRepository is not available on web.');
 }

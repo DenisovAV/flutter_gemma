f0312d39fff8fef3ec91efb9d5c52e6601e684e6
Sasha Denisov <denisov.shureg@gmail.com>
2026-05-30 16:10:32 +0200

    0.16.3: fix Android GPU sampler DT_NEEDED (patchelf libLiteRtLm.so, #270)

--- hook/build.dart
+++ hook/build.dart
@@ -172,7 +172,7 @@ const _litertlmBundle = _NativeBundle(
     'litertlm-ios_sim_arm64.tar.gz':
         '54e067fa11ad510280e01f90260e8bda13f905a27f00e7ebc2d7ef5847868bd1',
     'litertlm-android_arm64.tar.gz':
-        'ff43a1346a4a86d302bb83b2b4cdbf53c3fd3cefe3f433945145221bbd7cf2b1',
+        'f809c5a29867062cda74186c7ebebd500a2f36d0b6ad6f7ca8eab902af7fc784',
   },
   companions: [
     'GemmaModelConstraintProvider',
